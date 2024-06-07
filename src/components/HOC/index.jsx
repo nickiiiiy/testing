b8c27019ead6cfa55b499c6cc6b1e3466e3987c9
Nickolay <snsarkisyan@yandex.ru>
2024-06-07 09:05:57 +0300

    hoc

--- src/components/HOC/index.jsx
+++ src/components/HOC/index.jsx
@@ -1,21 +1,70 @@
-import { Navigate, useLocation } from "react-router-dom";
+// import { Navigate, useLocation } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// const withAuth = (WrappedComponent) => {
+//   const AuthWrapper = () => {
+//     const isAuth = useSelector((state) => state.user.isAuth);
+//     const location = useLocation();
+
+//     if (!isAuth) {
+//       return (
+//         <Navigate to="/authorization" replace state={{ from: location }} />
+//       );
+//     }
+
+//     return <WrappedComponent />;
+//   };
+
+//   return AuthWrapper;
+// };
+
+// export default withAuth;
+
+
+// WithAuthCheck.js
+import React from "react";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-const withAuth = (WrappedComponent) => {
-  const AuthWrapper = () => {
-    const isAuth = useSelector((state) => state.user.isAuth);
-    const location = useLocation();
+const WithAuthCheck = ({ component }) => {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const token = localStorage.getItem("token");
 
-    if (!isAuth) {
-      return (
-        <Navigate to="/authorization" replace state={{ from: location }} />
-      );
-    }
+  if (!isAuth || !token) {
+    return <Navigate to="/authorization" replace />;
+  }
 
-    return <WrappedComponent />;
-  };
-
-  return AuthWrapper;
+  return component;
 };
 
-export default withAuth;
+export default WithAuthCheck;
+
+// App.js
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Registration from "./components/pages/Registration";
+import Authorization from "./components/pages/Authorization";
+import Main from "./components/pages/Main";
+import WithAuthCheck from "./components/HOC";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path="/authorization"
+        element={<Authorization />}
+      />
+      <Route
+        path="/registration"
+        element={<Registration />}
+      />
+      <Route
+        path="/"
+        element={<WithAuthCheck component={<Main />} />}
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
+
+export default App;
